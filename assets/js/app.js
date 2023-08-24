2c789a9659fb3c1885c5b5eaebf35d2c3774be64
Camilo Tacue Salazar <cetacue@misena.edu.co>
2023-08-24 01:21:27 -0500

    10%

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,26 @@
-import {motos,datosBusqueda,max,min,resultado} from '../modulos/modulos.js'
-//-------------------------------------------------------------------------
+import { rutas, divMostrar } from '../modulos/modulos.js';
 
-document.addEventListener('DOMContentLoaded',()=>{
-    // mostrarmotos();
-})
-//------------------------------------------------------------
-function mostrarmotos(motos){
-    motos.forEach((element) => {
-        const motoHTML=document.createElement('p');
-        motoHTML.innerHTML=`<p>${element.linea}</p>`
-    });
+function mostrarRuta(nombre) {
+    const rutaSeleccionada = rutas.find(ruta => ruta.nombre === nombre);
 
+    divMostrar.innerHTML = '';
+
+    if (rutaSeleccionada) {
+        const rutaHTML = document.createElement('p');
+        rutaHTML.textContent = `Nombre: ${rutaSeleccionada.nombre}
+                                Kilometros:${rutaSeleccionada.Kilometros}
+                                Estado: ${rutaSeleccionada.estado}
+                                Calificación: ${rutaSeleccionada.calificacion}
+                                Tipo de Via: ${rutaSeleccionada.tipoVia}`;
+        divMostrar.appendChild(rutaHTML);
+    } else {
+        const mensaje = document.createElement('p');
+        mensaje.textContent = "Ruta no encontrada.";
+        divMostrar.appendChild(mensaje);
+    }
 }
+
+document.querySelector('#select1').addEventListener('change', function() {
+    const nombreRuta = this.value;
+    mostrarRuta(nombreRuta);
+});
